bce00a70b2a90d3b0c7b9531f2209ed1aeda5cdf
Leo Vigna <leo.vigna@gmail.com>
2022-06-24 23:31:53 -0400

    refactor

--- packages/demo-components/src/components/index.ts
+++
@@ -1,3 +0,0 @@
-export { AppTitle } from './AppTitle/AppTitle';
-export { Button } from './Button/Button';
-export { Web3LatestBlockNo } from './Web3LatestBlockNo/Web3LatestBlockNo';
--- packages/demo-components/src/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './components';
-export { getEnvironment, setEnvironment } from './environment';
