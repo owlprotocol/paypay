f2057bab44d63d81c2413bbad9674d6dd06b9c3b
Clarence Liu <clarence.hl.liu@gmail.com>
2022-06-25 21:45:54 -0400

    Adds WAGMI injected connector

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -4,10 +4,28 @@ import styled from 'styled-components';
 import { HeaderNav } from './components';
 import { LoanList } from './screens';
 
+import { WagmiConfig, createClient, chain, configureChains } from 'wagmi'
+
 import '@fontsource/manrope/400.css';
 import '@fontsource/manrope/500.css';
 import '@fontsource/manrope/600.css';
 import '@fontsource/manrope/800.css';
+
+import { infuraProvider } from 'wagmi/providers/infura';
+import { InjectedConnector } from 'wagmi/connectors/injected';
+
+const infuraId = '66117717d0b044a2a8c7fe221a0c0000';
+
+const { chains, provider } = configureChains(
+    [chain.rinkeby],
+    [infuraProvider({ infuraId })],
+)
+
+const wagmiClient = createClient({
+    autoConnect: true,
+    connectors: [new InjectedConnector({ chains })],
+    provider,
+});
 
 const AppWrapper = styled.div`
     padding: 0 24px;
@@ -15,12 +33,14 @@ const AppWrapper = styled.div`
 
 function App() {
     return (
-        <AppWrapper>
-            <Container maxW="container.xl">
-                <HeaderNav />
-                <LoanList />
-            </Container>
-        </AppWrapper>
+        <WagmiConfig client={wagmiClient}>
+            <AppWrapper>
+                <Container maxW="container.xl">
+                    <HeaderNav />
+                    <LoanList />
+                </Container>
+            </AppWrapper>
+        </WagmiConfig>
     );
 }
 
